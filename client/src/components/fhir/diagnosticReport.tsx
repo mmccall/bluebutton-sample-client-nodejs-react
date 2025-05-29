ea048f6bb18111ba46218bf3bc94ff71d988dbd6
Matt McCall <matt@mattmccall.com>
2025-05-29 12:28:06 -0400

    add diagnostic report.

---
+++ client/src/components/fhir/diagnosticReport.tsx
@@ -0,0 +1,139 @@
+import { Table, TableBody, TableCaption, TableRow, TableCell, TableHead, Tabs, TabPanel } from '@cmsgov/design-system';
+import React, { useEffect, useState } from 'react';
+import * as process from 'process';
+import ReactJson from 'react-json-view';
+import { DiagnosticReport } from 'fhir/r4';
+
+export type FHIRRecord = {
+    fullUrl: string,
+    resource: DiagnosticReport,
+}
+
+export type ErrorResponse = {
+    type: string,
+    content: string,
+}
+
+export default function FHIRDiagnosticReport() {
+    const [coverageRecords, setCoverageRecords] = useState<FHIRRecord[]>([]);
+    const [message, setMessage] = useState<ErrorResponse>();
+
+    useEffect(() => {
+        const test_url = process.env.TEST_APP_API_URL ? process.env.TEST_APP_API_URL : '';
+
+        console.log('hi');
+
+        // get coverage data
+        fetch(`${test_url}/api/data/diagnosticReport`)
+            .then(res => {
+                console.log(res);
+                return res.json();
+            }).then(fhirData => {
+                console.log('wat');
+                console.log(fhirData);
+                if (fhirData.resourceType === "Bundle") {
+                    const records: FHIRRecord[] = fhirData.entry.map((resourceData: any) => {
+                        return {
+                            fullUrl: resourceData.fullUrl,
+                            resource: resourceData.resource
+                        }
+                    });
+                    setCoverageRecords(records);
+                }
+                else {
+                    if (fhirData.resourceType === "OperationOutcome") {
+                        console.log(fhirData.issue);
+                        setMessage({ "type": "error", "content": fhirData.message || "Unknown" })
+                    }
+                }
+            });
+    }, [])
+
+    if (message) {
+        return (
+            <div className='full-width-card'>
+                <Table className="ds-u-margin-top--2" stackable stackableBreakpoint="md">
+                    <TableCaption>Error Response</TableCaption>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell id="column_1">Type</TableCell>
+                            <TableCell id="column_2">Content</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell stackedTitle="Type" headers="column_1">
+                                {message.type}
+                            </TableCell>
+                            <TableCell stackedTitle="Content" headers="column_2">
+                                {message.content}
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                {coverageRecords.map(record => {
+                    return (
+                        <div className="ds-l-col--6 ds-u-margin--2">
+                            <Tabs>
+                                <TabPanel key="display" id="display" tab="Display">
+                                    <Table>
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell
+                                                    key="Description"
+                                                    id="description"
+                                                >
+                                                    Description
+                                                </TableCell>
+                                                <TableCell
+                                                    key="Value"
+                                                    id="value"
+                                                >
+                                                    Value
+                                                </TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableRow>
+                                            <TableCell>
+                                               Diagnostic Test:
+                                            </TableCell>
+                                            <TableCell>
+                                               {record.resource.code.text}
+                                            </TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell>
+                                                Test Issued:
+                                            </TableCell>
+                                            <TableCell>
+                                                {record.resource.issued}
+                                            </TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell>
+                                                Result Reference:
+                                            </TableCell>
+                                            <TableCell>
+                                               {record.resource.category?.[0].text}
+                                            </TableCell>
+                                        </TableRow>
+                                    </Table>
+                                </TabPanel>
+                                <TabPanel key="source" id="source" tab="Source">
+                                    <div style={{ minHeight: '230px' }}>
+                                        <ReactJson src={record} collapsed={true} />
+                                    </div>
+                                </TabPanel>
+                            </Tabs>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    };
+}
--- client/src/components/records.tsx
+++ client/src/components/records.tsx
@@ -5,6 +5,7 @@ import * as process from 'process';
 import FHIRPatient from './fhir/patient';
 import FHIRCoverage from './fhir/coverage';
 import FHIRCondition from './fhir/condition';
+import FHIRDiagnosticReport from './fhir/diagnosticReport';
 
 export type EOBRecord = {
     id: string,
@@ -182,6 +183,9 @@ export default function Records() {
                 <FHIRCoverage />
                 <h1>Condition Entries</h1>
                 <FHIRCondition />        
+                <h1>Diagnostic Report Entries</h1>
+                <FHIRDiagnosticReport />        
+                
                 <h2>Observations</h2>
                 <div className="ds-u-display--flex ds-u-flex-direction--column ds-u-lg-flex-direction--row ds-u-flex-wrap--nowrap ds-u-lg-flex-wrap--wrap">
                     
