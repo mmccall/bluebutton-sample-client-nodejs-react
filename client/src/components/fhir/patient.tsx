eb2005d5469c45f74d5672d57daf295919eb2e9d
Matt McCall <matt@mattmccall.com>
2025-05-30 15:00:27 -0400

    refactor patient.

--- client/src/components/fhir/patient.tsx
+++ client/src/components/fhir/patient.tsx
@@ -1,76 +1,138 @@
-import { Table, TableRow, TableCell, TableHead, Tabs, TabPanel } from '@cmsgov/design-system';
+import { Table, TableBody, TableCaption, TableRow, TableCell, TableHead, Tabs, TabPanel } from '@cmsgov/design-system';
+import React, { useEffect, useState } from 'react';
+import * as process from 'process';
 import ReactJson from 'react-json-view';
+import { Patient } from 'fhir/r4';
 
-export type PatientResource = {
-    name: string[],
-    birthDate: string,
-    id: string
-}
-
-export type PatientRoot = {
+export type FHIRRecord = {
     fullUrl: string,
-    resource: PatientResource[],
+    resource: Patient,
 }
 
-export default function FHIRPatient( {patientData} ) {
-    return (
-        <div className="ds-u-display--flex ds-u-flex-direction--column ds-u-lg-flex-direction--row ds-u-flex-wrap--nowrap ds-u-lg-flex-wrap--wrap">
+export type ErrorResponse = {
+    type: string,
+    content: string,
+}
 
-            {patientData.map(record => {
-                return (
-                    <div className="default-card ds-u-margin--2" >
-                        <Tabs>
-                            <TabPanel key="display" id="display" tab="Display">
-                                <Table>
-                                    <TableHead>
+export default function FHIRPatient() {
+    const [bundleRecords, setBundleRecords] = useState<FHIRRecord[]>([]);
+    const [bundleCount, setBundleCount] = useState(0)
+    const [message, setMessage] = useState<ErrorResponse>();
+
+    useEffect(() => {
+        const test_url = process.env.TEST_APP_API_URL ? process.env.TEST_APP_API_URL : '';
+
+        // get coverage data
+        fetch(`${test_url}/api/data/patient`)
+            .then(res => {
+                return res.json();
+            }).then(fhirData => {
+                console.log(fhirData);
+                if (fhirData.resourceType === "Bundle") {
+                    setBundleCount(fhirData.total)
+                    const records: FHIRRecord[] = fhirData.entry.map((resourceData: any) => {
+                        return {
+                            fullUrl: resourceData.fullUrl,
+                            resource: resourceData.resource
+                        }
+                    });
+                    setBundleRecords(records);
+                }
+                else {
+                    if (fhirData.resourceType === "OperationOutcome") {
+                        console.log(fhirData.issue);
+                        setMessage({ "type": "error", "content": fhirData.message || "Unknown" })
+                    }
+                }
+            });
+    }, [])
+
+    if (message) {
+        return (
+            <div className='full-width-card'>
+                <Table className="ds-u-margin-top--2" stackable stackableBreakpoint="md">
+                    <TableCaption>Error Response</TableCaption>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell id="column_1">Type</TableCell>
+                            <TableCell id="column_2">Content</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell stackedTitle="Type" headers="column_1">
+                                {message.type}
+                            </TableCell>
+                            <TableCell stackedTitle="Content" headers="column_2">
+                                {message.content}
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
+        );
+    } else {
+        return (
+            <div className="ds-u-display--flex ds-u-flex-direction--column ds-u-lg-flex-direction--row ds-u-flex-wrap--nowrap ds-u-lg-flex-wrap--wrap">
+                <div className="ds-l-col--12"><h2>Total Records: {bundleCount}</h2></div>
+                {bundleRecords.map(record => {
+                    return (
+                        <div className="default-card ds-u-margin--2">
+                            <Tabs>
+                                <TabPanel key="display" id="display" tab="Display">
+                                    <Table>
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell
+                                                    key="Description"
+                                                    id="description"
+                                                >
+                                                    Description
+                                                </TableCell>
+                                                <TableCell
+                                                    key="Value"
+                                                    id="value"
+                                                >
+                                                    Value
+                                                </TableCell>
+                                            </TableRow>
+                                        </TableHead>
                                         <TableRow>
-                                            <TableCell
-                                                key="Description"
-                                                id="description"
-                                            >
-                                                Description
+                                            <TableCell>
+                                               Name:
                                             </TableCell>
-                                            <TableCell
-                                                key="Value"
-                                                id="value"
-                                            >
-                                                Value
+                                            <TableCell>
+                                               {record.resource.name?.[0].text}
                                             </TableCell>
                                         </TableRow>
-                                    </TableHead>
-                                    <TableRow>
-                                        <TableCell>
-                                            Name:
-                                        </TableCell>
-                                        <TableCell>
-                                            {record.resource.name[0].text}
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>
-                                            Date of Birth:
-                                        </TableCell>
-                                        <TableCell>
-                                            {record.resource.birthDate}
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>
-                                            Identifier:
-                                        </TableCell>
-                                        <TableCell>
-                                            {record.resource.id}
-                                        </TableCell>
-                                    </TableRow>
-                                </Table>
-                            </TabPanel>
-                            <TabPanel key="source" id="source" tab="Source">
-                                <ReactJson src={record} collapsed={true} />
-                            </TabPanel>
-                        </Tabs>
-                    </div>
-                )
-            })}
-        </div>
-    );
+                                        <TableRow>
+                                            <TableCell>
+                                                Date of Birth:
+                                            </TableCell>
+                                            <TableCell>
+                                              {record.resource.birthDate}
+                                            </TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell>
+                                                Primary ID:
+                                            </TableCell>
+                                            <TableCell>
+                                               {record.resource.id}
+                                            </TableCell>
+                                        </TableRow>
+                                    </Table>
+                                </TabPanel>
+                                <TabPanel key="source" id="source" tab="Source">
+                                    <div style={{ minHeight: '230px' }}>
+                                        <ReactJson src={record} collapsed={true} />
+                                    </div>
+                                </TabPanel>
+                            </Tabs>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    };
 }
--- client/src/components/records.tsx
+++ client/src/components/records.tsx
@@ -1,7 +1,4 @@
-import { Table, TableCaption, TableRow, TableCell, TableHead, TableBody, Tabs, TabPanel } from '@cmsgov/design-system';
-import React, { useEffect, useState } from 'react';
-import ReactJson from 'react-json-view';
-import * as process from 'process';
+import { Tabs, TabPanel } from '@cmsgov/design-system';
 import FHIRPatient from './fhir/patient';
 import FHIRCoverage from './fhir/coverage';
 import FHIRCondition from './fhir/condition';
@@ -20,181 +17,16 @@ import FHIRPractitionerRole from './fhir/practitionerRole';
 import FHIRProcedure from './fhir/procedure';
 import FHIRServiceRequest from './fhir/serviceRequest';
 
-export type EOBRecord = {
-    id: string,
-    code: string,
-    display: string,
-    amount: number
-}
-
-export type PatientRecord = {
-    fullUrl: string,
-    resource: string,
-}
-
-export type FHIRRecord = {
-    fullUrl: string,
-    resource: string,
-}
-
-export type ErrorResponse = {
-    type: string,
-    content: string,
-}
-
-export default function Records() {
-    //const [records, setRecords] = useState<EOBRecord[]>([]);
-    const [patientRecords, setPatientRecords] = useState<PatientRecord[]>([]);
-    const [observationRecords, setObservationRecords] = useState<PatientRecord[]>([]);
-    const [message, setMessage] = useState<ErrorResponse>();
-    /*
-    * DEVELOPER NOTES:
-    *  Here we are parsing through the different PDE Claim records
-    * for the user/beneficiary.  We have hard coded certain pieces of this data. (ie...item[0])
-    * You will want to find a method of parsing the FHIR JSON response to get the data
-    * you need for your application.  Don't forget to use a 'Discriminator' to determine
-    * which item within a list you want to get data from.
-    * 
-    * ie.  You are interested in getting all Prescription Drug NDC codes you would use the following criteria/discriminator
-    * resource.item[N].coding[N].code WHERE resource.item[N].coding[N].system = "http://hl7.org/fhir/sid/ndc"
-    * 
-    * 
-    * *NOTE* 
-    * There are multiple claim types within the BB2 Sandbox, not just PDE (Part-D Events - Drug/Medication Claims).  There are also
-    * Carrier Claims, SNF, HHA, Hospice, Inpatient, and Outpatient
-    */
-    useEffect(() => {
-        const test_url = process.env.TEST_APP_API_URL ? process.env.TEST_APP_API_URL : '';
-
-
-        // get eob data
-        /**
-        fetch(`${test_url}/api/data/benefit`)
-            .then(res => {
-                return res.json();
-            }).then(eobData => {
-                if (eobData.entry) {
-                    const records: EOBRecord[] = eobData.entry.map((resourceData: any) => {
-                        const resource = resourceData.resource;
-                        return {
-                            id: resource.id,
-                            code: resource.item[0]?.productOrService?.coding[0]?.code || 'Unknown',
-                            display: resource.item[0]?.productOrService?.coding[0]?.display || 'Unknown Prescription Drug',
-                            amount: resource.item[0]?.adjudication[7]?.amount?.value || '0'
-                        }
-                    });
-                    setRecords(records);
-                }
-                else {
-                    if (eobData.message) {
-                        setMessage({ "type": "error", "content": eobData.message || "Unknown" })
-                    }
-                }
-            });
-             */
-
-        // get coverage data
-        fetch(`${test_url}/api/data/coverage`)
-        .then(res => {
-            return res.json();
-        }).then(fhirData => {
-            if (fhirData.entry) {
-                const records: FHIRRecord[] = fhirData.entry.map((resourceData: any) => {
-                    return {
-                        fullUrl: resourceData.fullUrl,
-                        resource: resourceData.resource
-                    }
-                });
-                setCoverageRecords(records);
-            }
-            else {
-                if (fhirData.message) {
-                    setMessage({ "type": "error", "content": fhirData.message || "Unknown" })
-                }
-            }
-        });
-
-        // get patient data
-        fetch(`${test_url}/api/data/patient`)
-            .then(res => {
-                return res.json();
-            }).then(fhirData => {
-                if (fhirData.entry) {
-                    const records: PatientRecord[] = fhirData.entry.map((resourceData: any) => {
-                        return {
-                            fullUrl: resourceData.fullUrl,
-                            resource: resourceData.resource
-                        }
-                    });
-                    setPatientRecords(records);
-                }
-                else {
-                    if (fhirData.message) {
-                        setMessage({ "type": "error", "content": fhirData.message || "Unknown" })
-                    }
-                }
-            });
-
-        // get observation data
-        fetch(`${test_url}/api/data/observation`)
-            .then(res => {
-                return res.json();
-            }).then(fhirData => {
-                if (fhirData.entry) {
-                    const records: PatientRecord[] = fhirData.entry.map((resourceData: any) => {
-                        return {
-                            fullUrl: resourceData.fullUrl,
-                            resource: resourceData.resource
-                        }
-                    });
-                    setObservationRecords(records);
-                }
-                else {
-                    if (fhirData.message) {
-                        setMessage({ "type": "error", "content": fhirData.message || "Unknown" })
-                    }
-                }
-            });
-    }, [])
-
-
-
-    if (message) {
+export default function Records() { 
+       
         return (
-            <div className='full-width-card'>
-                <Table className="ds-u-margin-top--2" stackable stackableBreakpoint="md">
-                    <TableCaption>Error Response</TableCaption>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell id="column_1">Type</TableCell>
-                            <TableCell id="column_2">Content</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell stackedTitle="Type" headers="column_1">
-                                {message.type}
-                            </TableCell>
-                            <TableCell stackedTitle="Content" headers="column_2">
-                                {message.content}
-                            </TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </div>
-        );
-    } else {
-        return (
-            
-
-
             <div className='ds-content'>
                 
                 <Tabs tablistClassName="ds-u-margin-top--3">
                     <TabPanel id="patient" tab="Patient Records">
                       <div>
                         <h2>Patient Entries</h2>
-                        <FHIRPatient patientData={patientRecords}/>               
+                        <FHIRPatient />               
                       </div>
                 </TabPanel>
                 <TabPanel id="clinical" tab="Clinical Records">
@@ -245,4 +77,4 @@ export default function Records() {
             </div>
         );
     }
-}
+
